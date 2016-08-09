9839cacf2eaece666250cc9b354b4c15d705b38c
Michael Holtzscher <michael.holtzscher@cerner.com>
2016-08-09 08:50:52 -0500

    Created helper to style table rows in students index

--- app/helpers/students_helper.rb
+++ app/helpers/students_helper.rb
@@ -1,2 +1,11 @@
 module StudentsHelper
+    def account_status(student)
+        if student.balance > student.rate
+            "active"
+        elsif student.balance >= 0
+            "warning"
+        else
+            "danger"
+        end
+    end
 end
