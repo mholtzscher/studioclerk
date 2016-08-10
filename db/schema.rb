10e17efb29ad0743c4507cf598a4519e993201dc
Michael Holtzscher <thenerdproject1@gmail.com>
2016-08-10 10:42:12 -0500

    Added devise authentication.

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20160809034624) do
+ActiveRecord::Schema.define(version: 20160810144927) do
 
   create_table "credits", force: :cascade do |t|
     t.decimal  "ammount"
@@ -43,6 +43,23 @@ ActiveRecord::Schema.define(version: 20160809034624) do
     t.string   "parents_email"
     t.datetime "created_at",    null: false
     t.datetime "updated_at",    null: false
+  end
+
+  create_table "users", force: :cascade do |t|
+    t.string   "email",                  default: "", null: false
+    t.string   "encrypted_password",     default: "", null: false
+    t.string   "reset_password_token"
+    t.datetime "reset_password_sent_at"
+    t.datetime "remember_created_at"
+    t.integer  "sign_in_count",          default: 0,  null: false
+    t.datetime "current_sign_in_at"
+    t.datetime "last_sign_in_at"
+    t.string   "current_sign_in_ip"
+    t.string   "last_sign_in_ip"
+    t.datetime "created_at",                          null: false
+    t.datetime "updated_at",                          null: false
+    t.index ["email"], name: "index_users_on_email", unique: true
+    t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
   end
 
 end
