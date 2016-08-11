669d42b1647e12af68459cb036c9b3404a0c8bb2
Michael Holtzscher <michael.holtzscher@cerner.com>
2016-08-11 13:55:33 -0500

    devise config for mailer.

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -66,6 +66,10 @@ Rails.application.configure do
     :enable_starttls_auto => true
   }
 
+  ActionMailer::Base.delivery_method = :smtp
+
+  config.action_mailer.default_url_options = { :host => 'studioclerk.herokuapp.com' }
+
   # Ignore bad email addresses and do not raise email delivery errors.
   # Set this to true and configure the email server for immediate delivery to raise delivery errors.
   # config.action_mailer.raise_delivery_errors = false
