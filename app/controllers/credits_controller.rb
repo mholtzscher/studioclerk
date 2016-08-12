e050cd0a909818c9db178e1f795535b3ef033fc5
Michael Holtzscher <michael.holtzscher@cerner.com>
2016-08-12 11:04:40 -0500

    debug

--- app/controllers/credits_controller.rb
+++ app/controllers/credits_controller.rb
@@ -29,6 +29,7 @@ class CreditsController < ApplicationController
       if @credit.save
         credit_account
 
+        puts params[:email_receipt].type
         if params[:email_receipt].to_i == 1
           CreditMailer.credit_email(@student, @credit).deliver_now
         end
