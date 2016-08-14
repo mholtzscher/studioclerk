eade5957d53aca4fafecd6326ecf16b9af91d4ea
Michael Holtzscher <thenerdproject1@gmail.com>
2016-08-14 17:24:01 -0500

    Form validation for lesson and credit

--- app/models/credit.rb
+++ app/models/credit.rb
@@ -2,4 +2,5 @@ class Credit < ApplicationRecord
     belongs_to :student
     scope :recent, -> { order(date_time: :desc).limit(6) }
     attr_accessor :email_receipt
+    validates :date_time, :amount, :presence => true
 end
--- app/models/lesson.rb
+++ app/models/lesson.rb
@@ -2,4 +2,5 @@ class Lesson < ApplicationRecord
     belongs_to :student
     scope :recent, -> { order(date_time: :desc).limit(6) }
     attr_accessor :email_receipt
+    validates :date_time, :duration, :presence => true
 end
