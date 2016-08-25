3a3b992d2fda4ca1e9fbffdc1729d2a597de4b16
Michael Holtzscher <michael.holtzscher@cerner.com>
2016-08-25 11:00:22 -0500

    removed more heroku

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -62,7 +62,7 @@ Rails.application.configure do
     :authentication => :plain,
     :user_name      => ENV['SENDGRID_USERNAME'],
     :password       => ENV['SENDGRID_PASSWORD'],
-    :domain         => 'heroku.com',
+    :domain         => 'studioclerkapp.com',
     :enable_starttls_auto => true
   }
 
