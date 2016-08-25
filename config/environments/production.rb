400830551c2b15f3bc8ee4ba77cd7e2fc9692784
Michael Holtzscher <michael.holtzscher@cerner.com>
2016-08-25 11:04:00 -0500

    setup sendgrid secrets

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -60,8 +60,8 @@ Rails.application.configure do
     :address        => 'smtp.sendgrid.net',
     :port           => '587',
     :authentication => :plain,
-    :user_name      => ENV['SENDGRID_USERNAME'],
-    :password       => ENV['SENDGRID_PASSWORD'],
+    :user_name      => Rails.application.secrets.sendgrid_username,
+    :password       => Rails.application.secrets.sendgrid_password,
     :domain         => 'studioclerkapp.com',
     :enable_starttls_auto => true
   }
