2fd01f546283bf4ad8d776ca11abc1f2c36baf04
Michael Holtzscher <thenerdproject1@gmail.com>
2016-08-27 18:52:01 -0500

    Reverting lesson time changes

--- db/migrate/20160827182829_add_lesson_time_to_students.rb
+++
@@ -1,6 +0,0 @@
-class AddLessonTimeToStudents < ActiveRecord::Migration[5.0]
-  def change
-    add_column :students, :lesson_day, :integer, default: 0
-    add_column :students, :lesson_time, :time
-  end
-end
