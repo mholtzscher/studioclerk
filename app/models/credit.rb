d3de328128c945f5a386b3efe4ba6877d3551cf6
Michael Holtzscher <michael.holtzscher@cerner.com>
2016-08-30 14:13:39 -0500

    rubocop clean up of credit model

--- app/models/credit.rb
+++ app/models/credit.rb
@@ -1,7 +1,8 @@
+# Model for Account Credits
 class Credit < ApplicationRecord
-    belongs_to :student
-    scope :recent, -> { order(date_time: :desc).limit(6) }
-    scope :new_to_old, -> { order(date_time: :desc) }
-    attr_accessor :email_receipt
-    validates :date_time, :amount, :presence => true
+  belongs_to :student
+  scope :recent, -> { order(date_time: :desc).limit(6) }
+  scope :new_to_old, -> { order(date_time: :desc) }
+  attr_accessor :email_receipt
+  validates :date_time, :amount, presence: true
 end
