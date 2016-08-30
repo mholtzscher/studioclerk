ce848eb2584b5f01f3bded11bc8462e9662ab902
Michael Holtzscher <michael.holtzscher@cerner.com>
2016-08-30 14:13:30 -0500

    add comment to student model

--- app/models/student.rb
+++ app/models/student.rb
@@ -1,16 +1,17 @@
+# Student model
 class Student < ApplicationRecord
-    belongs_to :user
-    has_many :lessons, dependent: :destroy
-    has_many :credits, dependent: :destroy
-    validates :name, :balance, :rate, presence: true
+  belongs_to :user
+  has_many :lessons, dependent: :destroy
+  has_many :credits, dependent: :destroy
+  validates :name, :balance, :rate, presence: true
 
-    def account_debit(cost)
-        self.balance -= cost
-        self.save
-    end
+  def account_debit(cost)
+    self.balance -= cost
+    self.save
+  end
 
-    def account_credit(amount)
-        self.balance += amount
-        self.save
-    end
+  def account_credit(amount)
+    self.balance += amount
+    self.save
+  end
 end
