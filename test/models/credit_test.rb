ba52f3006bb2917d5e991756da1be2eb037cbd75
Michael Holtzscher <thenerdproject1@gmail.com>
2016-08-31 22:50:55 -0500

    credit model tests

--- test/models/credit_test.rb
+++ test/models/credit_test.rb
@@ -4,4 +4,10 @@ class CreditTest < ActiveSupport::TestCase
   # test "the truth" do
   #   assert true
   # end
+
+  should belong_to(:student)
+  should validate_presence_of(:date_time)
+  should validate_presence_of(:amount)
+  should allow_mass_assignment_of(:email_receipt)
+
 end
