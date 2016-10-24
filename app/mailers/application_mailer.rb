5428f31e84e2b2548fa7b9abe4848b10d350532d
Michael Holtzscher <michael.holtzscher@cerner.com>
2016-10-24 15:21:18 -0500

    Add local_time helper to mailers

--- app/mailers/application_mailer.rb
+++ app/mailers/application_mailer.rb
@@ -2,4 +2,8 @@
 class ApplicationMailer < ActionMailer::Base
   default from: 'from@example.com'
   layout 'mailer'
+
+  def local_time date_time
+        date_time.localtime.to_formatted_s(:custom_long_ordinal)
+    end
 end
