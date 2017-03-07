2bf2a3a19aae2e9744aaa178873be4f73501275c
Michael Holtzscher <thenerdproject1@gmail.com>
2017-03-06 20:44:20 -0600

    stop converting local time and use system times

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,6 +1,6 @@
 # Helpers for shared application level
 module ApplicationHelper
   def local_time(date_time)
-    date_time.localtime.to_formatted_s(:custom_long_ordinal)
+    date_time.to_formatted_s(:custom_long_ordinal)
   end
 end
